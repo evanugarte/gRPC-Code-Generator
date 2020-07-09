17c0e07e184d4f62058cc776bd1490cdef9fc3ae
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2020-07-08 23:32:06 -0700

    Stub API Generate Endpoint (#13)

--- ec2/server.py
+++ ec2/server.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import uuid
+import os
 
 api = Flask(__name__)
 
 
 @api.route('/', methods=['GET'])
-def get_companies():
+def handle_health_check():
+    return 'Hello I work!'
+
+
+@api.route('/generate', methods=['POST'])
+def generate_grpc_code():
     return 'Hello I work!'
 
 
